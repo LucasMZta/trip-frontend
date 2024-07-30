2eeda7c31f1aef8bddc4185eb9fd8fa67344300a
Lucas Martins <lucas2zonta@hotmail.com>
2024-07-30 11:58:41 -0300

    Update env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,5 +2,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-   baseURL: process.env.API_BASE_URL
+   // baseURL: process.env.API_BASE_URL
+   baseURL: 'https://trip-backend-2453763a8b77.herokuapp.com'
 })
